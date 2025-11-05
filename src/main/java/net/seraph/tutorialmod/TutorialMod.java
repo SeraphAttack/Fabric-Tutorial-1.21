4eeb8abccb3634b648d4dac56bd1e0d6dd43f3a8
Natalie <npvincent1987@gmail.com>
2025-11-05 14:56:30 -0700

    Added custom block groups! woo!

--- src/main/java/net/seraph/tutorialmod/TutorialMod.java
+++ src/main/java/net/seraph/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.seraph.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.seraph.tutorialmod.block.ModBlocks;
+import net.seraph.tutorialmod.item.ModItemGroups;
 import net.seraph.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
 	}
