a957d73beb37a6ae59333983e036afe5867cf6ab
Natalie <npvincent1987@gmail.com>
2025-11-07 11:02:54 -0700

    Added custom advanced items! woo!

--- src/main/java/net/seraph/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/seraph/tutorialmod/item/ModItemGroups.java
@@ -18,6 +18,8 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
+
+                        entries.add(ModItems.CHISEL);
                     }).build());
     public static final ItemGroup PINK_GARNET_BlOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(TutorialMod.MOD_ID,"pink_garnet_blocks"),
