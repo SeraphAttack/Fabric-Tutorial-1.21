b46034d7ed0bd9fc49c14699eb2ca07bfd25b7ac
Natalie <npvincent1987@gmail.com>
2025-11-07 20:05:44 -0700

    Added custom data gen! woo!

---
+++ src/main/java/net/seraph/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,21 @@
+package net.seraph.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.seraph.tutorialmod.item.ModItems;
+import net.seraph.tutorialmod.util.ModTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.RAW_PINK_GARNET);
+    }
+}
