b8e2a971e5a71236004162f61cd7f9d2c28b2609
Natalie <npvincent1987@gmail.com>
2025-11-10 14:57:32 -0700

    Added sheep fucking! woo!

--- src/main/java/net/seraph/tutorialmod/TutorialMod.java
+++ src/main/java/net/seraph/tutorialmod/TutorialMod.java
@@ -2,8 +2,16 @@ package net.seraph.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.EndRodBlock;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.seraph.tutorialmod.block.ModBlocks;
 import net.seraph.tutorialmod.component.ModDataComponentTypes;
 import net.seraph.tutorialmod.item.ModItemGroups;
@@ -28,5 +36,16 @@ public class TutorialMod implements ModInitializer {
         FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 
         PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (entity instanceof SheepEntity sheepEntity && !world.isClient) {
+                if (player.getMainHandStack().getItem() == Items.END_ROD) {
+                    player.sendMessage(Text.literal("I FUCK SHEEP!!!"));
+                    player.getMainHandStack().decrement(1);
+                    sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600));
+                }
+                return ActionResult.PASS;
+            }
+            return ActionResult.PASS;
+        });
 	}
 }
